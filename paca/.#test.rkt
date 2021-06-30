24395fa92d6bfbf8cc926a78927b9add29f20e92
Phage Ky <kyp0717@gmail.com>
2021-06-29 20:15:57 -0400

    fix websocket

---
+++ paca/.#test.rkt
@@ -0,0 +1,1 @@
+phage@rack-vm.321
--- paca/market-iex.rkt
+++ paca/market-iex.rkt
@@ -8,21 +8,24 @@
          json
          net/rfc6455)
 
+(define (recv/print c)
+  (printf "Got message: ~a\n" (ws-recv c)))
+
 (define (iex/send conn req)
-  (ws-send! conn req)
-  (ws-recv conn))
+  (ws-send! conn req #:flush? #t)
+  (recv/print conn))
 
 ;;; connect to iex
 (define protocol 'rfc6455)
 (define iex/conn (ws-connect (string->url urliex) #:protocol protocol))
-
+(recv/print iex/conn)
 ;;; authenticate
 ;; {"action": "auth", "key": "{APCA-API-KEY-ID}", "secret": "{APCA-API-SECRET-KEY}"}
 (define iex-req/secret
   (jsexpr->string  (hasheq 'action "auth" 'key key 'secret secret)))
 
 
-(define iex-resp/auth (string->jsexpr (iex/send iex/conn iex/secret)))
+(define iex-resp/auth (string->jsexpr (iex/send iex/conn iex-req/secret)))
 
 ;;; get quotes
 (define tickers '("AMD" "MSFT"))
